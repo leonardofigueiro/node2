24c756a3eb12e8da330b9a8f8972b9234d0201bd
Leonardo Figueiró <leonardofigueiro@MacBook-Pro-de-Leonardo.local>
2022-12-13 16:45:35 -0400

    Finalização

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,7 +1,8 @@
 import mongoose from "mongoose";
-
+import * as dotenv from 'dotenv';
+dotenv.config()
 // mongoose.set('strictQuery', true)
-mongoose.connect("mongodb+srv://leofigueiro:1234@cluster0.vzfl9sy.mongodb.net/alura-node")
+mongoose.connect(`mongodb+srv://${process.env.USUARIO}:${process.env.PASWD}@cluster0.vzfl9sy.mongodb.net/alura-node`)
 
 let db = mongoose.connection;
 
